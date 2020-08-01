9c854ea24c8bfee6b2005ee8e84bbd481c0a81bf
shreesh123 <shreeshsrvstv@gmail.com>
2020-08-01 18:01:35 +0530

    Adding the condition accourding to the given condition * Adding genre table * Editing command as per the requirement

--- tracks.py
+++ tracks.py
@@ -9,8 +9,13 @@ cur.executescript('''
 DROP TABLE IF EXISTS Artist;
 DROP TABLE IF EXISTS Album;
 DROP TABLE IF EXISTS Track;
-
+DROP TABLE IF EXISTS Genre;
 CREATE TABLE Artist (
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    name    TEXT UNIQUE
+);
+
+CREATE TABLE Genre (
     id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
     name    TEXT UNIQUE
 );
@@ -22,10 +27,11 @@ CREATE TABLE Album (
 );
 
 CREATE TABLE Track (
-    id  INTEGER NOT NULL PRIMARY KEY 
+    id  INTEGER NOT NULL PRIMARY KEY
         AUTOINCREMENT UNIQUE,
     title TEXT  UNIQUE,
     album_id  INTEGER,
+    genre_id  INTEGER,
     len INTEGER, rating INTEGER, count INTEGER
 );
 ''')
@@ -57,25 +63,34 @@ for entry in all:
     count = lookup(entry, 'Play Count')
     rating = lookup(entry, 'Rating')
     length = lookup(entry, 'Total Time')
+    genre = lookup(entry, 'Genre')
 
-    if name is None or artist is None or album is None : 
+    # print(name , artist , album , count , rating , length)
+
+    if name is None or artist is None or album is None or genre is None :
         continue
 
-    print(name, artist, album, count, rating, length)
+    # print(name, artist, album, count, rating, length)
 
-    cur.execute('''INSERT OR IGNORE INTO Artist (name) 
+    cur.execute('''INSERT OR IGNORE INTO Artist (name)
         VALUES ( ? )''', ( artist, ) )
     cur.execute('SELECT id FROM Artist WHERE name = ? ', (artist, ))
     artist_id = cur.fetchone()[0]
 
-    cur.execute('''INSERT OR IGNORE INTO Album (title, artist_id) 
+    cur.execute('''INSERT OR IGNORE INTO Album (title, artist_id)
         VALUES ( ?, ? )''', ( album, artist_id ) )
     cur.execute('SELECT id FROM Album WHERE title = ? ', (album, ))
     album_id = cur.fetchone()[0]
 
+    cur.execute('''INSERT OR IGNORE INTO Genre (name)
+        VALUES ( ? )''', ( genre, ) )
+    cur.execute('SELECT id FROM Genre WHERE name = ? ', (genre, ))
+    genre_id = cur.fetchone()[0]
+
     cur.execute('''INSERT OR REPLACE INTO Track
-        (title, album_id, len, rating, count) 
-        VALUES ( ?, ?, ?, ?, ? )''', 
-        ( name, album_id, length, rating, count ) )
+        (title, album_id, genre_id, len, rating, count)
+        VALUES ( ?, ?, ?, ?, ?, ?)''',
+        ( name, album_id, genre_id, length, rating, count ) )
 
     conn.commit()
+print("End of Program")
